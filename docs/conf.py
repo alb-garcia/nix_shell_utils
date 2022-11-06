ac7ad0f7a7c08ecad21b327b734bd4dd24537108
Alberto Garcia <beto.garcia.izquierdo@gmail.com>
2022-11-06 09:47:55 +0100

    more

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-extensions = [
+extensions = ['loco'
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
