932f00572a1eea60292b9494799d086850b6fef2
Alberto Garcia <beto.garcia.izquierdo@gmail.com>
2022-11-06 16:37:15 +0100

    dev change up

---
+++ clean.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from nix_shell_utils import *
+
+rm('*~')
+with cd('docs'): rm('*~')
+with cd('nix_shell_utils'): rm('*~')
+
+
+    
+
+    
---
+++ gpush.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from nix_shell_utils import *
+
+runc('git status')
+runc('git add .')
+runc("git commit -m 'dev change up'")
+    
+
+    
