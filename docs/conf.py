3104d6a0de34f76f802c116aeb624fe6877f6084
Alberto Garcia <beto.garcia.izquierdo@gmail.com>
2022-11-06 10:58:23 +0100

    more

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('../nix_shell_utils'))
+
 sys.path.insert(0, os.path.abspath('..'))
-#sys.path.insert(0, os.path.abspath('.'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -32,7 +32,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # -- Options for autodoc ----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
